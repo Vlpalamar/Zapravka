1517026dda74750ac0b9d2208503c5c06ce31008
Vlpalamar <65705755+Vlpalamar@users.noreply.github.com>
2020-10-29 13:31:34 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PriceForThisFuel = new System.Windows.Forms.TextBox();
+            this.FuelTypes = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LitterForPrice = new System.Windows.Forms.TextBox();
+            this.PriceForLiter = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FullPriceLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.FullCafeLabel = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PriceForThisFuel);
+            this.panel1.Controls.Add(this.FuelTypes);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.LitterForPrice);
+            this.panel1.Controls.Add(this.PriceForLiter);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(45, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(1, 0, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(234, 286);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // PriceForThisFuel
+            // 
+            this.PriceForThisFuel.Enabled = false;
+            this.PriceForThisFuel.Location = new System.Drawing.Point(94, 71);
+            this.PriceForThisFuel.Name = "PriceForThisFuel";
+            this.PriceForThisFuel.Size = new System.Drawing.Size(103, 20);
+            this.PriceForThisFuel.TabIndex = 8;
+            this.PriceForThisFuel.TextChanged += new System.EventHandler(this.PriceForThisFuel_TextChanged);
+            // 
+            // FuelTypes
+            // 
+            this.FuelTypes.FormattingEnabled = true;
+            this.FuelTypes.Location = new System.Drawing.Point(94, 31);
+            this.FuelTypes.Name = "FuelTypes";
+            this.FuelTypes.Size = new System.Drawing.Size(103, 21);
+            this.FuelTypes.TabIndex = 1;
+            this.FuelTypes.SelectedIndexChanged += new System.EventHandler(this.FuelTypes_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(204, 78);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "грн.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(204, 136);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "л.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(204, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "грн.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(29, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "к оплате";
+            // 
+            // LitterForPrice
+            // 
+            this.LitterForPrice.Enabled = false;
+            this.LitterForPrice.Location = new System.Drawing.Point(125, 152);
+            this.LitterForPrice.Name = "LitterForPrice";
+            this.LitterForPrice.Size = new System.Drawing.Size(72, 20);
+            this.LitterForPrice.TabIndex = 4;
+            this.LitterForPrice.TextChanged += new System.EventHandler(this.LitterForPrice_TextChanged);
+            // 
+            // PriceForLiter
+            // 
+            this.PriceForLiter.Enabled = false;
+            this.PriceForLiter.Location = new System.Drawing.Point(125, 129);
+            this.PriceForLiter.Name = "PriceForLiter";
+            this.PriceForLiter.Size = new System.Drawing.Size(72, 20);
+            this.PriceForLiter.TabIndex = 1;
+            this.PriceForLiter.TextChanged += new System.EventHandler(this.PriceForLiter_TextChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.FullPriceLabel);
+            this.panel3.Location = new System.Drawing.Point(15, 210);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(203, 71);
+            this.panel3.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(169, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "грн.";
+            // 
+            // FullPriceLabel
+            // 
+            this.FullPriceLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FullPriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FullPriceLabel.Location = new System.Drawing.Point(84, 16);
+            this.FullPriceLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.FullPriceLabel.Name = "FullPriceLabel";
+            this.FullPriceLabel.Size = new System.Drawing.Size(92, 46);
+            this.FullPriceLabel.TabIndex = 1;
+            this.FullPriceLabel.Text = "00,00";
+            this.FullPriceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.FullPriceLabel.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Location = new System.Drawing.Point(15, 125);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(93, 51);
+            this.panel2.TabIndex = 2;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(5, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "сумма";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(5, 5);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "количество";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "цена";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "бензин";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(60, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Автозаправка";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label13);
+            this.panel5.Controls.Add(this.FullCafeLabel);
+            this.panel5.Location = new System.Drawing.Point(42, 210);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(203, 71);
+            this.panel5.TabIndex = 3;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(169, 42);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(27, 13);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "грн.";
+            // 
+            // FullCafeLabel
+            // 
+            this.FullCafeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FullCafeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FullCafeLabel.Location = new System.Drawing.Point(84, 16);
+            this.FullCafeLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.FullCafeLabel.Name = "FullCafeLabel";
+            this.FullCafeLabel.Size = new System.Drawing.Size(92, 46);
+            this.FullCafeLabel.TabIndex = 1;
+            this.FullCafeLabel.Text = "00,00";
+            this.FullCafeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label12.Location = new System.Drawing.Point(56, 205);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(51, 13);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "к оплате";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.panel5);
+            this.groupBox1.Location = new System.Drawing.Point(305, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 286);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Мини Кафэ";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // panel4
+            // 
+            this.panel4.AutoScroll = true;
+            this.panel4.Location = new System.Drawing.Point(19, 39);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(267, 87);
+            this.panel4.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(857, 490);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PriceForLiter;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label4;
+        //private System.Windows.Forms.TextBox FuelPrice;
+        private System.Windows.Forms.TextBox LitterForPrice;
+        private System.Windows.Forms.Label FullPriceLabel;
+        private System.Windows.Forms.ComboBox FuelTypes;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PriceForThisFuel;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label FullCafeLabel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        List<Gas> gas;
+        List<Product> products;
+
+        List<CheckBox> checkBoxes;
+        List<TextBox> textBoxes;
+        List<NumericUpDown> numericUpDowns;
+
+
+
+        double FuelPrice;
+        double FullFuelPrice;
+        decimal FullCafePrice = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            gas = new List<Gas>
+            {
+                new Gas{Name="A-95",Price =22.50f },
+                new Gas{Name="A-98",Price =28.30f },
+                new Gas{Name="Disel",Price =18.10f },
+                new Gas{Name="A-98.Premiums",Price =15.80f },
+                new Gas{Name="Гас",Price =10.00f }
+            };
+            products = new List<Product>
+            {
+                new Product{Name="Хот-дог",Price =5.50f },
+                new Product{Name="Гамбургер",Price =7.20f },
+                new Product{Name="Картошка Фри",Price =3.10f },
+                new Product{Name="Кока-Кола",Price =10.00f }
+
+            };
+            foreach (var item in gas)
+            {
+                FuelTypes.Items.Add(item.Name);
+            }
+            FuelTypes.SelectedIndex = 0;
+            radioButton1.Checked = true;
+
+
+            checkBoxes = new List<CheckBox>(products.Count);
+            textBoxes = new List<TextBox>(products.Count);
+            numericUpDowns = new List<NumericUpDown>(products.Count);
+           
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                CheckBox checkBox = new CheckBox();
+                checkBox.Location = new System.Drawing.Point(16, 14 + 30 * i);
+                checkBox.Size = new System.Drawing.Size(80, 17);
+                checkBox.CheckedChanged += checkBox_checkedChanged;
+                checkBox.Text = products[i].Name;
+                panel4.Controls.Add(checkBox);
+                checkBoxes.Add(checkBox);
+
+                TextBox textBox = new TextBox();
+                textBox.Size = new System.Drawing.Size(42, 20);
+                textBox.Location = new System.Drawing.Point(118, 11 + 30 * i);
+                textBox.Text = products[i].Price.ToString();
+                textBox.Enabled = false;
+                panel4.Controls.Add(textBox);
+                textBoxes.Add(textBox);
+
+                NumericUpDown numericUpDown = new NumericUpDown();
+                numericUpDown.Size = new System.Drawing.Size(55, 20);
+                numericUpDown.Location = new System.Drawing.Point(181, 11 + 30 * i);
+                numericUpDown.Enabled = false;
+                numericUpDown.ValueChanged += numericUpDown_ValueChanged;
+                panel4.Controls.Add(numericUpDown);
+                numericUpDowns.Add(numericUpDown);
+
+                decimal Price = new decimal();
+               
+            }
+        }
+        class Gas
+        {
+            public string Name { get; set; }
+            public float Price { get; set; }
+            public Gas() { }
+        }
+        class Product
+        {
+            public string Name { get; set; }
+            public float Price { get; set; }
+            public Product() { }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkBox_checkedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox.Checked==true)
+            {
+                numericUpDowns[checkBoxes.IndexOf(checkBox)].Enabled = true;
+            }
+            else
+            {
+                numericUpDowns[checkBoxes.IndexOf(checkBox)].Enabled = false;
+                numericUpDowns[checkBoxes.IndexOf(checkBox)].Value = 0;
+            }
+           
+
+        }
+      
+        private void numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            FullCafePrice = 0;
+
+            NumericUpDown numericUpDown = sender as NumericUpDown;
+            for (int i = 0; i < products.Count; i++)
+            {
+                FullCafePrice  += numericUpDowns[i].Value * decimal.Parse(textBoxes[i].Text);
+            }
+           
+            GetCafeTotalSumm();
+         
+        }
+        private void FuelTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PriceForThisFuel.Text = gas[FuelTypes.SelectedIndex].Price.ToString();
+            FuelPrice =Convert.ToDouble(PriceForThisFuel.Text);
+            radioButton1.Checked = true;
+            PriceForLiter.Text = "";
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked==true)
+            {
+                this.PriceForLiter.TextChanged += new System.EventHandler(this.PriceForLiter_TextChanged);
+                this.LitterForPrice.TextChanged -= new System.EventHandler(this.LitterForPrice_TextChanged);
+            }
+            else
+            {
+                this.PriceForLiter.TextChanged -= new System.EventHandler(this.PriceForLiter_TextChanged);
+                this.LitterForPrice.TextChanged += new System.EventHandler(this.LitterForPrice_TextChanged);
+            }
+            if (radioButton1.Enabled)
+            {
+                PriceForLiter.Enabled=true;
+                LitterForPrice.Enabled = false;
+                LitterForPrice.Text="";
+                PriceForLiter.Text = "";
+            }
+           
+
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                this.PriceForLiter.TextChanged -= new System.EventHandler(this.PriceForLiter_TextChanged);
+                this.LitterForPrice.TextChanged += new System.EventHandler(this.LitterForPrice_TextChanged);
+            }
+            else
+            {
+                this.PriceForLiter.TextChanged += new System.EventHandler(this.PriceForLiter_TextChanged);
+                this.LitterForPrice.TextChanged -= new System.EventHandler(this.LitterForPrice_TextChanged);
+            }
+            if (radioButton2.Enabled)
+            {
+                PriceForLiter.Enabled = false;
+                LitterForPrice.Enabled = true;
+                LitterForPrice.Text = "";
+                PriceForLiter.Text = "";
+            }
+        
+
+
+        }
+
+       
+
+        private void PriceForThisFuel_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void PriceForLiter_TextChanged(object sender, EventArgs e)
+        {
+            if(PriceForLiter.Text!="")
+            {
+               FullFuelPrice = Convert.ToDouble(FuelPrice * Convert.ToDouble(PriceForLiter.Text));
+            }
+            else
+            {
+                FullFuelPrice = 00.00d;
+            }
+            LitterForPrice.Text = Convert.ToString(FullFuelPrice);
+            GetFuelTotalSumm();
+
+
+        }
+
+        private void LitterForPrice_TextChanged(object sender, EventArgs e)
+        {
+            if (LitterForPrice.Text != "")
+            {
+                FullFuelPrice = Convert.ToDouble(LitterForPrice.Text);
+            }
+            else
+            {
+                FullFuelPrice = 00.00d;
+            }
+            if (LitterForPrice.Text!="0"&&LitterForPrice.Text!="")
+            {
+                PriceForLiter.Text = Convert.ToString(FullFuelPrice / gas[FuelTypes.SelectedIndex].Price);
+            }
+            GetFuelTotalSumm();
+
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+  
+
+       
+       
+        void GetFuelTotalSumm(float discond = 1f)
+        {
+            FullFuelPrice = FullFuelPrice * discond;
+            FullPriceLabel.Text = Convert.ToString(FullFuelPrice);
+        }
+        void GetCafeTotalSumm(decimal discond = 1M)
+        {
+            FullCafePrice = FullCafePrice * discond;
+            FullCafeLabel.Text = Convert.ToString(FullCafePrice);
+        }
+        
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
